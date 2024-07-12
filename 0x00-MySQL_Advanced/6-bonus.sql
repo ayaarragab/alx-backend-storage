64f4aabb9c2c28bfab5eaf8248532472468f540c
ayaarragab <ayarsk4343@gmail.com>
2024-07-12 04:08:43 +0300

    add bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,8 +3,12 @@
 DELIMITER //
 CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
+    DECLARE project_id INT;
+    IF(SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF; 
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
     INSERT INTO corrections (`user_id`, `project_name`, `score`) VALUES (user_id, project_name, score);
 END //
 DELIMITER ;
-
-CALL AddBonus();
