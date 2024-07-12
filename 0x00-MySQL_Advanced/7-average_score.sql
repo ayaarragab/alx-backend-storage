8cdf68141adc5d5e24b9bf0a9daf95c76ec954dd
ayaarragab <ayarsk4343@gmail.com>
2024-07-12 04:18:09 +0300

    add bonus

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,14 @@
+-- creation of users table
+-- columns: id, email, name
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_Scores FLOAT;
+    SET avg_Scores = SELECT AVG(score) FROM corrections AS c WHERE c.user_id = user_id;
+    UPDATE users SET average_score = avg_Scores WHERE id = user_id;
+END //
+
+DELIMITER ;
