8cceb8ca92fc34785baad61bc77ac01490728a3f
ayaarragab <ayarsk4343@gmail.com>
2024-07-12 04:50:52 +0300

    101

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -2,18 +2,23 @@
 -- columns: id, email, name
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-    DECLARE avg_scores FLOAT;
-    SET avg_scores = (
-        SELECT SUM(c.score * p.weight) / SUM(p.weight)
-        FROM corrections AS c
-        JOIN projects AS p ON c.project_id = p.id
-        WHERE c.user_id = user_id
-    );
-    UPDATE users SET average_score = avg_scores WHERE id = user_id;
+    DECLARE LEN_STUDENTS INT;
+    SET LEN_STUDENTS = (SELECT COUNT(*) FROM users);
+    DECLARE counter INT DEFAULT 0;
+    REPEAT
+        DECLARE avg_scores FLOAT;
+        SET avg_scores = (
+            SELECT SUM(c.score * p.weight) / SUM(p.weight)
+            FROM corrections AS c
+            JOIN projects AS p ON c.project_id = p.id
+            WHERE c.user_id = counter
+        );
+        UPDATE users SET average_score = avg_scores WHERE id = counter;
+        SET counter = counter + 1;
+    UNTIL counter >= LEN_STUDENTS;
+    END REPEAT;
 END //
 
 DELIMITER ;
