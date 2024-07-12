de0cb248d0c5c33831a465b0f1e95549a18db931
ayaarragab <ayarsk4343@gmail.com>
2024-07-12 04:10:57 +0300

    add bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,14 +1,20 @@
 -- creation of users table
 -- columns: id, email, name
 DELIMITER //
-CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
+
+CREATE PROCEDURE AddBonus (
+    IN user_id INT, 
+    IN project_name VARCHAR(255), 
+    IN score INT
+)
 BEGIN
     DECLARE project_id INT;
-    IF(SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
-    THEN
+
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 THEN
         INSERT INTO projects (name) VALUES (project_name);
-    END IF; 
+    END IF;
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (`user_id`, `project_name`, `score`) VALUES (user_id, project_name, score);
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END //
+
 DELIMITER ;
