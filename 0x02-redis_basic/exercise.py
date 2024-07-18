b952d60cae91b4fcc6f8535976fa47fdb475c51e
ayaarragab <ayarsk4343@gmail.com>
2024-07-18 03:24:30 +0300

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Cache class Module excercise
+"""
+import redis
+from uuid import uuid4
+import random
+
+
+class Cache:
+    """
+    Cache class
+    """
+
+    def __init__(self: object):
+        """
+        store an instance
+        of the Redis client as a private
+        variable named _redis
+        """
+        self.redisObj: redis.Redis = redis.Redis()
+        self.redisObj.flushdb()
+
+    def store(self: object, data: str | bytes | int | float) -> str:
+        """
+        store method that takes a data argument
+        and returns a string.
+        The method should generate a random key (e.g. using uuid)
+        store the input data in Redis using the random key
+        and return the key.
+        """
+        key: str = str(uuid4())
+        self.redisObj.set(key, data)
+        return key
