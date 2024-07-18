59fd582ba3e29c3768bb6bcf79317d54bea5b826
ayaarragab <ayarsk4343@gmail.com>
2024-07-18 19:15:50 +0300

    call history decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,29 @@ from typing import Union, Callable, TypeVar, Optional
 from functools import wraps
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    decorator that takes a single method Callable
+    argument and returns a Callable
+    """
+    methodName = method.__qualname__
+    inputs = methodName + ":inputs"
+    outputs = methodName + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        function that increments the count for that
+        key every time the method is called and returns
+        the value returned by the original method.
+        """
+        self._redis.rpush(inputs, *args)
+        outut_data = method(*args, **kwargs)
+        self._redis.rpush(outputs, outut_data)
+        return outut_data
+    return wrapper
+
+
 def count_calls(method: Callable) -> Callable:
     """
     decorator that takes a single method Callable
@@ -40,6 +63,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
