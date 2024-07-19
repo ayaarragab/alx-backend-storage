e78f3e6b3706743732a6220c22093067f1818f68
ayaarragab <ayarsk4343@gmail.com>
2024-07-19 14:20:09 +0300

    update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python function that changes all topics
+of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Python function that changes all topics
+    of a school document based on the name
+    """
+    myquery = { "name": name }
+    newValues = {'$set':{'topics':topics}}
+    mongo_collection.update_one(myquery, newValues)
