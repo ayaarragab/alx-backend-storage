8d3b81c374a4e5a19241f14cad12c1524fdadfee
ayaarragab <ayarsk4343@gmail.com>
2024-07-19 14:23:16 +0300

    updateMany

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -12,4 +12,4 @@ def update_topics(mongo_collection, name, topics):
     """
     myquery = { "name": name }
     newValues = {'$set':{'topics':topics}}
-    mongo_collection.update(myquery, newValues)
+    mongo_collection.updateMany(myquery, newValues)
