f246df11426130b9b1ae223b490c093ca10422ee
ayaarragab <ayarsk4343@gmail.com>
2024-07-19 14:11:28 +0300

    delelte

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Python function that inserts
+a new document in a collection based on kwarg
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Python function that inserts
+    a new document in a collection based on kwarg
+    """
+    mongo_collection.insert_many(**kwargs)
+    return kwargs['id']
