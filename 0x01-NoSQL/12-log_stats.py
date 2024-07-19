c7e2ef7de5932d61ec827576a94b46218746f25e
ayaarragab <ayarsk4343@gmail.com>
2024-07-19 15:46:50 +0300

    this file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 a Python script that provides some
-stats about Nginx logs stored in MongoDB:
+statsabout Nginx logs stored in MongoDB:
 """
 from pymongo import MongoClient
 
